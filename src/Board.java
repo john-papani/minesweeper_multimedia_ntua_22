c613595f4256d70bed3218388fce8d3b22a2f289
john-papani <johnpapani1@gmail.com>
2022-11-22 15:13:47 +0200

    last commit before javafx

--- src/Board.java
+++ src/Board.java
@@ -5,27 +5,45 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.io.FilenameFilter;
+import java.awt.image.ImageObserver;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.Random;
+
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.text.html.StyleSheet;
 
-public class Board extends JPanel {
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 
-    private final int NUM_IMAGES = 13;
+public class Board extends JLabel {
+
+    private final int NUM_IMAGES = 16;
     private final int CELL_SIZE = 15;
 
     private final int COVER_FOR_CELL = 10;
     private final int FOR_FLAG = 10;
     private final int EMPTY_CELL = 0;
     private final int MINE_CELL = 9;
-    private final int FOR_SUPERBOMB_CELL = 11;
+    private final int FOR_SUPERBOMB_CELL = 21;
     private final int COVERED_MINE_CELL = MINE_CELL + COVER_FOR_CELL;
     private final int FLAGGED_MINE_CELL = COVERED_MINE_CELL + FOR_FLAG;
-    private final int SUPER_BOMB_CELL = COVERED_MINE_CELL + FOR_SUPERBOMB_CELL;
+    private final int SUPER_BOMB_CELL = MINE_CELL + FOR_SUPERBOMB_CELL;
+    private final int COVERED_SUPER_BOMB_CELL = SUPER_BOMB_CELL + COVER_FOR_CELL;
+    private final int FLAGGED_SUPER_BOMB = COVERED_SUPER_BOMB_CELL + FOR_FLAG;
+    private final int MINE_COL_LINE_OFSUPER = SUPER_BOMB_CELL + MINE_CELL;
 
     private final int DRAW_MINE = 9;
     private final int DRAW_COVER = 10;
@@ -33,8 +51,9 @@ public class Board extends JPanel {
     private final int DRAW_WRONG_FLAG = 12;
     private final int DRAW_MINE_SUPERBOMB_LINE_COL = 13;
     private final int DRAW_SUPERBOMB = 14;
+    private final int DRAW_FLAG_SUPERBOMB = 15;
 
-    private final int N_MINES = 10;
+    private final int N_MINES = 35;
     private final int N_ROWS = 15;
     private final int N_COLS = 15;
 
@@ -43,37 +62,104 @@ public class Board extends JPanel {
 
     private int[] field;
     private boolean inGame;
+    private boolean flagSuperBomb = false;
     private int flags;
     private Image[] img;
 
     private int successfulClicks = 0;
 
     private int allCells;
+    // private final Label statusBar;
+
+    // // private final Canvas canvas;
+    // // private final Pane boardPane;
     private final JLabel statusbar;
+    private String fileForMineTXT = "";
 
     public Board(JLabel statusbar) {
-
         this.statusbar = statusbar;
         initBoard();
     }
 
+    // public Board(Label statusBar, BorderPane bPane) {
+    // canvas = new Canvas(BOARD_WIDTH, BOARD_HEIGHT);
+    // this.statusBar = statusBar;
+    // bPane.setBottom(canvas);
+    // boardPane = bPane;
+    // initBoard();
+    // }
+
     private void loadImages() {
         img = new Image[NUM_IMAGES];
-
         for (int i = 0; i < NUM_IMAGES; i++) {
-
             var path = "src/img/" + i + ".png";
             img[i] = (new ImageIcon(path)).getImage();
         }
 
     }
 
-    private void initBoard() {
+    // private void loadImages() {
+    // Image[] img = new Image[NUM_IMAGES];
+    // for (int i = 0; i < NUM_IMAGES; i++) {
+    // var path = "src/img/" + i + ".png";
+    // try {
+    // img[i] = ImageIO.read(new FileInputStream(path));
+    // } catch (IOException e) {
+    // // TODO Auto-generated catch block
+    // e.printStackTrace();
+    // }
+    // }
+    // }
+
+    public void initBoard() {
 
         setPreferredSize(new Dimension(BOARD_WIDTH, BOARD_HEIGHT));
         loadImages();
+        // canvas.setOnMouseClicked(mousePressed());
         addMouseListener(new MinesAdapter());
         newGame();
+        // createBoard();
+    }
+
+    // private void createBoard() {
+    // // boardPane.getChildren().clear();
+    // boardPane.getHeight();
+    // for (int y = 0; y < canvas.getHeight(); y++) {
+    // for (int x = 0; x < canvas.getWidth(); x++) {
+    // Pane tilePane = new Pane();
+
+    // tilePane.setTranslateX(x * CELL_SIZE);
+    // tilePane.setTranslateY(y * CELL_SIZE);
+    // tilePane.setPrefWidth(CELL_SIZE);
+    // tilePane.setPrefHeight(CELL_SIZE);
+    // tilePane.getProperties().put("x", x);
+    // tilePane.getProperties().put("y", y);
+    // // tilePane.setOnMouseClicked(e -> mousePressed(e));
+
+    // ImageView tileImageView = new ImageView(img[0].toString());
+    // tilePane.getChildren().add(tileImageView);
+
+    // tilePane.setStyle("-fx-border-color: grey; -fx-border-width: 2px;
+    // -fx-background-color: lightgray;");
+
+    // boardPane.getChildren().add(tilePane);
+    // }
+    // }
+    // }
+
+    private void createMineTXT() {
+
+        SimpleDateFormat sf = new SimpleDateFormat("yyyy_MM_dd-HH_mm_ss");
+        Date date = new Date(System.currentTimeMillis());
+        var datetimenow = sf.format(date);
+        try (BufferedWriter writer = new BufferedWriter(
+                new FileWriter("./" + "mines" + datetimenow + ".txt", true))) {
+            writer.write(fileForMineTXT);
+            writer.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     private void newGame() {
@@ -102,50 +188,61 @@ public class Board extends JPanel {
             if ((position < allCells) && (field[position] != COVERED_MINE_CELL)) {
 
                 int current_col = position % N_COLS;
-                if (i == 0) {
-                    field[position] = SUPER_BOMB_CELL;
+                if (N_MINES > 34 && i == 0) {
+                    // the first one is the super-bomb
+                    field[position] = COVERED_SUPER_BOMB_CELL;
                 } else {
                     field[position] = COVERED_MINE_CELL;
                 }
+                int mineCol = position % N_COLS;
+                int mineRow = (position - mineCol) / N_ROWS;
+                int hasSuperBomb = i == 0 ? 1 : 0; // if is super-bomb, hasSuperBomb will be 1
+
+                // up + left is the 0,0 for the starting-location and the start is from 1 ( not
+                // zero)
+                String mineLocation = String.format("%d, %d, %d\n", mineRow + 1, mineCol + 1, hasSuperBomb);
+
+                fileForMineTXT += mineLocation;
                 i++;
                 if (current_col > 0) {
                     cell = position - 1 - N_COLS;
 
-                    if (cell >= 0 && field[cell] != COVERED_MINE_CELL && field[cell] != SUPER_BOMB_CELL)
+                    if (cell >= 0 && field[cell] != COVERED_MINE_CELL && field[cell] != COVERED_SUPER_BOMB_CELL)
                         field[cell] += 1;
 
                     cell = position - 1;
-                    if (cell >= 0 && field[cell] != COVERED_MINE_CELL && field[cell] != SUPER_BOMB_CELL)
+                    if (cell >= 0 && field[cell] != COVERED_MINE_CELL && field[cell] != COVERED_SUPER_BOMB_CELL)
                         field[cell] += 1;
 
                     cell = position + N_COLS - 1;
-                    if (cell < allCells && field[cell] != COVERED_MINE_CELL && field[cell] != SUPER_BOMB_CELL)
+                    if (cell < allCells && field[cell] != COVERED_MINE_CELL && field[cell] != COVERED_SUPER_BOMB_CELL)
                         field[cell] += 1;
                 }
 
                 cell = position - N_COLS;
-                if (cell >= 0 && field[cell] != COVERED_MINE_CELL && field[cell] != SUPER_BOMB_CELL)
+                if (cell >= 0 && field[cell] != COVERED_MINE_CELL && field[cell] != COVERED_SUPER_BOMB_CELL)
                     field[cell] += 1;
 
                 cell = position + N_COLS;
-                if (cell < allCells && field[cell] != COVERED_MINE_CELL && field[cell] != SUPER_BOMB_CELL)
+                if (cell < allCells && field[cell] != COVERED_MINE_CELL && field[cell] != COVERED_SUPER_BOMB_CELL)
                     field[cell] += 1;
 
                 if (current_col < (N_COLS - 1)) {
                     cell = position - N_COLS + 1;
-                    if (cell >= 0 && field[cell] != COVERED_MINE_CELL && field[cell] != SUPER_BOMB_CELL)
+                    if (cell >= 0 && field[cell] != COVERED_MINE_CELL && field[cell] != COVERED_SUPER_BOMB_CELL)
                         field[cell] += 1;
 
                     cell = position + N_COLS + 1;
-                    if (cell < allCells && field[cell] != COVERED_MINE_CELL && field[cell] != SUPER_BOMB_CELL)
+                    if (cell < allCells && field[cell] != COVERED_MINE_CELL && field[cell] != COVERED_SUPER_BOMB_CELL)
                         field[cell] += 1;
 
                     cell = position + 1;
-                    if (cell < allCells && field[cell] != COVERED_MINE_CELL && field[cell] != SUPER_BOMB_CELL)
+                    if (cell < allCells && field[cell] != COVERED_MINE_CELL && field[cell] != COVERED_SUPER_BOMB_CELL)
                         field[cell] += 1;
                 }
             }
         }
+        createMineTXT();
         for (int j = 0; j < allCells; j++) {
             System.out.print(field[j] + " ");
             if ((j + 1) % 15 == 0)
@@ -252,9 +349,10 @@ public class Board extends JPanel {
         int new_cell = cell - N_COLS;
         if (new_cell < allCells) {
             if (new_cell >= 0) {
-                if (field[new_cell] > MINE_CELL) {
+                if (field[new_cell] > MINE_CELL)
                     field[new_cell] -= COVER_FOR_CELL;
-                }
+                if (field[new_cell] == MINE_CELL)
+                    field[new_cell] = MINE_COL_LINE_OFSUPER;
                 open_up(new_cell);
             }
         }
@@ -265,21 +363,23 @@ public class Board extends JPanel {
         if (current_col > 0) {
             int new_cell = cell - 1;
             if (new_cell >= 0) {
-                if (field[new_cell] > MINE_CELL) {
+                if (field[new_cell] > MINE_CELL)
                     field[new_cell] -= COVER_FOR_CELL;
-                }
+                if (field[new_cell] == MINE_CELL)
+                    field[new_cell] = MINE_COL_LINE_OFSUPER;
                 open_left(new_cell);
             }
         }
     }
 
     private void open_down(int cell) {
-        
+
         int new_cell = cell + N_COLS;
         if (new_cell < allCells) {
-            if (field[new_cell] > MINE_CELL) {
+            if (field[new_cell] > MINE_CELL)
                 field[new_cell] -= COVER_FOR_CELL;
-            }
+            if (field[new_cell] == MINE_CELL)
+                field[new_cell] = MINE_COL_LINE_OFSUPER;
             open_down(new_cell);
         }
     }
@@ -294,6 +394,8 @@ public class Board extends JPanel {
                 if (field[new_cell] > MINE_CELL) {
                     field[new_cell] -= COVER_FOR_CELL;
                 }
+                if (field[new_cell] == MINE_CELL)
+                    field[new_cell] = MINE_COL_LINE_OFSUPER;
             }
         }
         if (current_col < (N_COLS - 1)) {
@@ -325,20 +427,32 @@ public class Board extends JPanel {
                 }
 
                 if (!inGame) {
-
                     if (cell == COVERED_MINE_CELL) {
                         cell = DRAW_MINE;
-                    } else if (cell == FLAGGED_MINE_CELL) {
+                    } else if (cell == FLAGGED_MINE_CELL || cell == FLAGGED_SUPER_BOMB) {
                         cell = DRAW_FLAG;
+                    } else if (cell == COVERED_SUPER_BOMB_CELL) {
+                        cell = DRAW_SUPERBOMB;
+                        // } else if (cell == SUPER_BOMB_CELL) {
+                        // cell = DRAW_SUPERBOMB;
+                    } else if (cell == MINE_COL_LINE_OFSUPER) {
+                        cell = DRAW_MINE_SUPERBOMB_LINE_COL;
                     } else if (cell > COVERED_MINE_CELL) {
                         cell = DRAW_WRONG_FLAG;
                     } else if (cell > MINE_CELL) {
                         cell = DRAW_COVER;
                     }
-
                 } else {
 
-                    if (cell > COVERED_MINE_CELL) {
+                    if (cell == SUPER_BOMB_CELL) {
+                        cell = DRAW_SUPERBOMB;
+                    } else if (cell == FLAGGED_MINE_CELL) {
+                        cell = DRAW_FLAG;
+                    } else if (cell == FLAGGED_SUPER_BOMB) {
+                        cell = DRAW_FLAG_SUPERBOMB;
+                    } else if (cell == MINE_COL_LINE_OFSUPER) {
+                        cell = DRAW_MINE_SUPERBOMB_LINE_COL;
+                    } else if (cell > COVERED_MINE_CELL && cell != COVERED_SUPER_BOMB_CELL) {
                         cell = DRAW_FLAG;
                     } else if (cell > MINE_CELL) {
                         cell = DRAW_COVER;
@@ -371,15 +485,26 @@ public class Board extends JPanel {
             int cCol = x / CELL_SIZE;
             int cRow = y / CELL_SIZE;
 
-            System.out.println("x " + x + " y " + y + " CCol " + cCol + "  Row " + cRow);
+            System.out.println("x " + x + " y " + y + " CCol " + cCol + " Row " + cRow);
             boolean doRepaint = false;
 
             // auto einai gia thn ennallagh anamesa sta paixnidia
             // tha prepei na to ftiaxo etsi oste na rotaei h na petaei ena munhma
-            if (!inGame) {
-                newGame();
-                repaint();
-            }
+
+            // String selection;
+            // if (!inGame) {
+            // Alert alert = new Alert(AlertType.CONFIRMATION, "Delete " + selection + " ?",
+            // ButtonType.YES,
+            // ButtonType.NO,
+            // ButtonType.CANCEL);
+            // alert.showAndWait();
+
+            // if (alert.getResult() == ButtonType.YES) {
+            // newGame();
+            // // repaint();
+            // }
+            // }
+            // ! TODO: NA SVISO TA PARAKATO
             for (int j = 0; j < allCells; j++) {
                 System.out.print(j + " ");
                 if ((j + 1) % N_COLS == 0)
@@ -398,11 +523,23 @@ public class Board extends JPanel {
                 // THIS IS FOR FLAGS
                 if (e.getButton() == MouseEvent.BUTTON3) {
                     successfulClicks++;
-                    if (field[(cRow * N_COLS) + cCol] > MINE_CELL) {
+                    if (field[(cRow * N_COLS) + cCol] > MINE_CELL
+                            && field[(cRow * N_COLS) + cCol] != MINE_COL_LINE_OFSUPER
+                            && field[(cRow * N_COLS) + cCol] != FLAGGED_SUPER_BOMB) {
 
                         doRepaint = true;
+                        System.out.println("succesu " + successfulClicks + " field = " + field[(cRow
+                                * N_COLS) + cCol]
+                                + " flagSuper " + flagSuperBomb);
+                        if (successfulClicks <= 4 && field[(cRow * N_COLS) + cCol] == COVERED_SUPER_BOMB_CELL
+                                && !flagSuperBomb) {
+                            flagSuperBomb = true;
+                            open_one_line_one_col((cRow * N_COLS) + cCol);
+                        }
 
-                        if (field[(cRow * N_COLS) + cCol] <= COVERED_MINE_CELL) {
+                        if ((field[(cRow * N_COLS) + cCol] <= COVERED_MINE_CELL
+                                || ((field[(cRow * N_COLS) + cCol] > SUPER_BOMB_CELL)
+                                        && field[(cRow * N_COLS) + cCol] <= COVERED_SUPER_BOMB_CELL))) {
 
                             if (flags > 0) {
                                 field[(cRow * N_COLS) + cCol] += FOR_FLAG;
@@ -420,37 +557,39 @@ public class Board extends JPanel {
                             statusbar.setText(msg);
                         }
                     }
-
                 }
                 // NOT A FLAG
-                // ! this if is for testing
+                // * this if is for testing
                 else if (e.getButton() == MouseEvent.BUTTON2) {
                     doRepaint = true;
+                    flagSuperBomb = true;
+                    System.out.print(flagSuperBomb);
                     field[(cRow * N_COLS) + cCol] -= COVER_FOR_CELL;
                     open_one_line_one_col((cRow * N_COLS) + cCol);
-
-                } else {
+                    flagSuperBomb = false;
+                    System.out.print(flagSuperBomb);
+                }
+                // NOT A FLAG
+                else {
 
                     if (field[(cRow * N_COLS) + cCol] > COVERED_MINE_CELL)
                         return;
 
-                    if (field[(cRow * N_COLS) + cCol] > MINE_CELL
-                            && field[(cRow * N_COLS) + cCol] < FLAGGED_MINE_CELL) {
+                    if ((field[(cRow * N_COLS) + cCol] > MINE_CELL
+                            && field[(cRow * N_COLS) + cCol] < FLAGGED_MINE_CELL)
+                            || (field[(cRow * N_COLS) + cCol] > SUPER_BOMB_CELL
+                                    && field[(cRow * N_COLS) + cCol] < FLAGGED_SUPER_BOMB)) {
 
                         field[(cRow * N_COLS) + cCol] -= COVER_FOR_CELL;
                         doRepaint = true;
 
-                        if (field[(cRow * N_COLS) + cCol] == MINE_CELL) {
+                        if (field[(cRow * N_COLS) + cCol] == MINE_CELL
+                                || field[(cRow * N_COLS) + cCol] == SUPER_BOMB_CELL) {
                             inGame = false;
                         }
 
                         if (field[(cRow * N_COLS) + cCol] == EMPTY_CELL) {
-                            if (successfulClicks <= 4 && field[(cRow * N_COLS) + cCol] == SUPER_BOMB_CELL)
-                                find_empty_cells((cRow * N_COLS) + cCol);
-                            // open_one_line_one_col((cRow * N_COLS) + cCol);
-                            else
-                                find_empty_cells((cRow * N_COLS) + cCol);
-
+                            find_empty_cells((cRow * N_COLS) + cCol);
                         }
                     }
                 }
@@ -460,5 +599,6 @@ public class Board extends JPanel {
                 }
             }
         }
+
     }
 }
--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -1,8 +1,38 @@
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.module.InvalidModuleDescriptorException;
+import java.util.List;
+
+import javax.swing.Action;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.plaf.metal.MetalBorders.PaletteBorder;
+
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.stage.Stage;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 
 /**
  * Java Minesweeper Game
@@ -11,13 +41,81 @@ import javax.swing.JLabel;
  * Website: http://zetcode.com
  */
 
+class InvalidDescriptionException extends Exception {
+    public InvalidDescriptionException(String errorMessage) {
+        super(errorMessage);
+    }
+}
+
+class InvalidValueException extends Exception {
+    public InvalidValueException(String errorMessage) {
+        super(errorMessage);
+    }
+}
+
 public class Minesweeper extends JFrame {
+    public static int difficulty;
+    public static int numMines;
+    public static int max_time;
+    public static int hasSuperMine;
+
+    private static int MIN_TIME_EASY = 120;
+    private static int MAX_TIME_EASY = 180;
+    private static int MIN_TIME_HARD = 240;
+    private static int MAX_TIME_HARD = 360;
+    private static int MIN_MINES_EASY = 9;
+    private static int MAX_MINES_EASY = 11;
+    private static int MIN_MINES_HARD = 35;
+    private static int MAX_MINES_HARD = 45;
 
     private JLabel statusbar;
 
     public Minesweeper() {
 
         initUI();
+    }
+
+    public static void readScenarioFile() throws Exception {
+        BufferedReader reader;
+        Integer[] varIntegers = { difficulty, numMines, max_time, hasSuperMine };
+        try {
+            reader = new BufferedReader(new FileReader(
+                    "src/medialab/SCENARIO-4.txt"));
+            int reader_len = 0;
+            String line = reader.readLine();
+
+            while (line != null) {
+                varIntegers[reader_len] = Integer.parseInt(line);
+                reader_len++;
+                System.out.println(line);
+                line = reader.readLine();
+            }
+            reader.close();
+            System.out.println("len " + reader_len);
+            if (reader_len != 4)
+                throw new InvalidDescriptionException("Incorrect lenght file : " +
+                        reader_len);
+            // easy and superbomb == that is incorrect
+            if (difficulty == 1 && hasSuperMine != 0)
+                throw new InvalidValueException("Incorect compination, because with easy can't have superbomb");
+            // number of mines out of limits
+            if ((difficulty == 1 && !(numMines > MIN_MINES_EASY && numMines < MAX_MINES_EASY))
+                    || (difficulty == 2 && !(numMines > MIN_MINES_HARD && numMines < MAX_MINES_HARD)))
+                throw new InvalidValueException("Incorect compination, because Mines out of limits");
+            // number of time out of limits
+            if ((difficulty == 1 && !(max_time > MIN_TIME_EASY && max_time < MAX_TIME_EASY))
+                    || (difficulty == 2 && !(max_time > MIN_TIME_HARD && max_time < MAX_TIME_HARD)))
+                throw new InvalidValueException("Incorect compination, because TIME out of     limits");
+        } catch (InvalidDescriptionException err) {
+            err.printStackTrace();
+            // throw;
+        } catch (InvalidValueException err) {
+            err.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+            // } finally {
+            // System.out.print("malakiaa egine");
+        }
     }
 
     private void initUI() {
@@ -37,10 +135,86 @@ public class Minesweeper extends JFrame {
 
     public static void main(String[] args) {
 
-        EventQueue.invokeLater(() -> {  
+        EventQueue.invokeLater(() -> {
 
             var ex = new Minesweeper();
             ex.setVisible(true);
         });
     }
 }
+
+// public class Minesweeper {
+
+// // private EventHandler<ActionEvent> startTheGame(Stage stage) {
+// // stage.close();
+// // }
+// Label statusBar = new Label("This will be the counter for flags");
+// BorderPane bPane = new BorderPane();
+// Scene scene = new Scene(bPane, 400, 400);
+
+// // Board Boardd = new Board(statusBar,bPane);
+// // // Boardd.N_COLS = 15;
+// // // Boardd.N_ROWS = 15;
+// // // Boardd.N_MINES = 3;
+// // // bPane.setBottom(Boardd);
+
+// // }
+
+// public static void main(String[] args) {
+// startGame();
+// // launch(args);
+// }
+
+// // @Override
+// // public void start(Stage stage) {
+// // Label mainGameHere = new Label("epp kaleeeppp kalispera, edo tha mpei to
+// // game");
+// // Button playEasyButton = new Button("Play Easy");
+// // Button playHardButton = new Button("Play Hard");
+
+// // playEasyButton.setOnAction(this::clicledButton);
+// // playHardButton.setOnAction(this::clicledhardButton);
+
+// // HBox hBox = new HBox();
+// // hBox.setSpacing(10);
+
+// // HBox.setMargin(playEasyButton, new Insets(20, 20, 20, 20));
+// // HBox.setMargin(playHardButton, new Insets(20, 20, 20, 20));
+
+// // ObservableList list = hBox.getChildren();
+// // list.addAll(playEasyButton, playHardButton);
+
+// // statusBar.setStyle("-fx-font-weight: bold");
+
+// // bPane.setTop(hBox);
+// // bPane.setCenter(mainGameHere);
+// // // bPane.setBottom(statusBar);
+// // BorderPane.setMargin(hBox, new Insets(10, 20, 10, 80));
+// // BorderPane.setMargin(mainGameHere, new Insets(20, 20, 20, 20));
+// // BorderPane.setMargin(statusBar, new Insets(20, 20, 5, 5));
+
+// // // BorderPane.setAlignment(hBox, Pos.BOTTOM_LEFT);
+
+// // stage.setTitle("O Narkalieutis Mou");
+// // stage.setScene(scene);
+
+// // initUI();
+
+// // stage.show();
+
+// // }
+
+// // private void clicledButton(ActionEvent e) {
+// // statusBar.setText("YESSS WE DID IT");
+// // }
+
+// // private void clicledhardButton(ActionEvent e) {
+// // statusBar.setText("hard nowwwwwwwwwwwwwwwww");
+// // }
+
+// // // public static void main(String[] args) throws Exception {
+// // // // readScenarioFile();
+// // // // startGame();
+// // // testingFx(null);
+
+// // // }
